1cef5dcccbc5065f87e66dbd6262c3d0ae235740
Christoph Sturm <me@christophsturm.com>
2021-02-17 23:42:04 +0100

    update dependencies and publish 0.1.3 to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,22 @@ import info.solidsoft.gradle.pitest.PitestPluginExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val junit5Version = "5.7.0"
-val junitPlatformVersion = "1.7.0"
-val kotlinVersion = "1.4.21-2"
+val junit5Version = "5.7.1"
+val junitPlatformVersion = "1.7.1"
+val kotlinVersion = "1.4.30"
 
 plugins {
     java
-    kotlin("jvm") version "1.4.21-2"
+    kotlin("jvm") version "1.4.30"
     id("com.github.ben-manes.versions") version "0.36.0"
     `maven-publish`
-    id("com.jfrog.bintray") version "1.8.5"
+    id("com.jfrog.bintray") version "1.8.4"
     id("info.solidsoft.pitest") version "1.5.2"
-
+    signing
 }
 
 group = "com.christophsturm"
-version = "0.1.2"
+version = "0.1.3"
 
 repositories {
     //    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
@@ -54,28 +54,61 @@ tasks {
             events("passed", "skipped", "failed")
         }
     }
-    create<Jar>("sourceJar") {
-        from(sourceSets.main.get().allSource)
-        archiveClassifier.set("sources")
-    }
 }
 
-artifacts {
-    add("archives", tasks["jar"])
-    add("archives", tasks["sourceJar"])
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
+
 
 publishing {
+    repositories {
+        maven {
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.properties["ossrhUsername"] as String?
+                password = project.properties["ossrhPassword"] as String?
+            }
+        }
+    }
+
+
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-            artifact(tasks["sourceJar"])
             groupId = project.group as String
             artifactId = "filepeek"
             version = project.version as String
+            pom {
+                description.set("A library to look up kotlin source files")
+                name.set("filepeek")
+                url.set("https://github.com/christophsturm/filepeek")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("christophsturm")
+                        name.set("Christoph Sturm")
+                        email.set("me@christophsturm.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/christophsturm/filepeek.git")
+                }
+            }
         }
     }
 }
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
 
 // BINTRAY_API_KEY= ... ./gradlew clean check publish bintrayUpload
 bintray {
@@ -105,7 +138,6 @@ plugins.withId("info.solidsoft.pitest") {
         outputFormats.set(setOf("XML", "HTML"))
     }
 }
-
 tasks.named<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>("dependencyUpdates") {
     val filtered =
         listOf("alpha", "beta", "rc", "cr", "m", "preview", "dev", "eap")
